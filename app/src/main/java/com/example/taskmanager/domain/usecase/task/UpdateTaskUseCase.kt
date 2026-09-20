3b626da3e8a077f756b865bbfedfacdac3874f38
brayan diaz <brayandiazarrieta4@gmail.com>
2026-09-20 18:03:46 -0500

    Editar tareas

---
+++ app/src/main/java/com/example/taskmanager/domain/usecase/task/UpdateTaskUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.taskmanager.domain.usecase.task
+
+import com.example.taskmanager.domain.model.Task
+import com.example.taskmanager.domain.repository.TaskRepository
+import javax.inject.Inject
+
+class UpdateTaskUseCase @Inject constructor(
+    private val taskRepository: TaskRepository
+) {
+    suspend operator fun invoke(
+        task: Task
+    ): Result<Task> {
+        return taskRepository.updateTask(task)
+    }
+}
