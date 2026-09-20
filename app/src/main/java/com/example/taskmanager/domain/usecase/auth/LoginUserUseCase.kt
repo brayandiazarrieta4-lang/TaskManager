cb439d657abff8c33feca1d05d5c389965365dc7
brayan diaz <brayandiazarrieta4@gmail.com>
2026-09-20 18:31:07 -0500

    Iniciar sesion UseCase

---
+++ app/src/main/java/com/example/taskmanager/domain/usecase/auth/LoginUserUseCase.kt
@@ -0,0 +1,21 @@
+package com.example.taskmanager.domain.usecase.auth
+
+import com.example.taskmanager.domain.model.User
+import com.example.taskmanager.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class LoginUserUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+
+    suspend operator fun invoke(
+        email: String,
+        password: String
+    ): Result<User> {
+
+        return authRepository.login(
+            email,
+            password
+        )
+    }
+}
